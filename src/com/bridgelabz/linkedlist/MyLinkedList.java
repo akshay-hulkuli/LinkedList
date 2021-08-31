508a64a6641887deb35578dec1f87a2e073109d5
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-31 18:54:45 +0530

    ADDED: add operation and display operation

---
+++ src/com/bridgelabz/linkedlist/MyLinkedList.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.linkedlist;
+
+public class MyLinkedList<K> {
+	public MyNode<K> head;
+	public MyNode<K> tail;
+	
+	public MyLinkedList() {
+		this.head = null;
+		this.tail = null;
+	}
+	
+	public void add(MyNode<K> newNode) {
+		if(head == null) {
+			this.head = newNode;
+		}
+		if(tail == null) {
+			this.tail = newNode;
+		}
+		else {
+			newNode.setNext(this.head);
+			this.head = newNode;
+		}
+	}
+	
+	public void display() {
+		MyNode<K> tempNode = this.head;
+		while(tempNode != null) {
+			System.out.print(tempNode.getKey()+" -> ");
+			tempNode = tempNode.getNext();
+		}
+		System.out.println("null");
+	}
+}
