e6de22c01bc195103bfa40097e7504522a1ee958
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-31 20:20:42 +0530

    REFACTORED: to call delete method

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -4,17 +4,19 @@ public class LinkedListMain {
 	public static void main(String[] args) {
 		MyNode<Integer> firstNode = new MyNode<Integer>(56);
 		MyNode<Integer> secondNode = new MyNode<Integer>(30);
-		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(40);
+		MyNode<Integer> fourthNode = new MyNode<Integer>(70);
 		
 		MyLinkedList<Integer> myLinkedList = new MyLinkedList<Integer>();
 		myLinkedList.append(firstNode);
 		myLinkedList.append(secondNode);
 		myLinkedList.append(thirdNode);
+		myLinkedList.append(fourthNode);
 		
 		System.out.println("Created a linked list :");
 		myLinkedList.display();
-		myLinkedList.searchAndInsert(30, 40);
-		System.out.println("after adding 40 after 30 : ");
-		myLinkedList.display();
+		myLinkedList.delete(40);
+		System.out.println("after deleting 40, size is : "+myLinkedList.size());
+		
 	}
 }
