c5977627edb9940f763a558b8ccb9ceee6f596ce
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-31 19:02:59 +0530

    REFACTORED: to append nodes

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -2,14 +2,14 @@ package com.bridgelabz.linkedlist;
 
 public class LinkedListMain {
 	public static void main(String[] args) {
-		MyNode<Integer> firstNode = new MyNode<Integer>(70);
+		MyNode<Integer> firstNode = new MyNode<Integer>(56);
 		MyNode<Integer> secondNode = new MyNode<Integer>(30);
-		MyNode<Integer> thirdNode = new MyNode<Integer>(56);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
 		
 		MyLinkedList<Integer> myLinkedList = new MyLinkedList<Integer>();
-		myLinkedList.add(firstNode);
-		myLinkedList.add(secondNode);
-		myLinkedList.add(thirdNode);
+		myLinkedList.append(firstNode);
+		myLinkedList.append(secondNode);
+		myLinkedList.append(thirdNode);
 		
 		System.out.println("Created a linked list :");
 		myLinkedList.display();
