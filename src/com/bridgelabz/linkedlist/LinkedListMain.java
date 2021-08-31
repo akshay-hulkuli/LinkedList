972614a51d62885dd82549e20a6866228feef36d
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-31 18:53:32 +0530

    ADDED:class that provides main method

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -2,19 +2,17 @@ package com.bridgelabz.linkedlist;
 
 public class LinkedListMain {
 	public static void main(String[] args) {
-		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		MyNode<Integer> firstNode = new MyNode<Integer>(70);
 		MyNode<Integer> secondNode = new MyNode<Integer>(30);
-		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(56);
 		
-		firstNode.setNext(secondNode);
-		secondNode.setNext(thirdNode);
+		MyLinkedList<Integer> myLinkedList = new MyLinkedList<Integer>();
+		myLinkedList.add(firstNode);
+		myLinkedList.add(secondNode);
+		myLinkedList.add(thirdNode);
 		
 		System.out.println("Created a linked list :");
-		MyNode<Integer> tempNode = firstNode;
-		while(tempNode != null) {
-			System.out.print(tempNode.getKey()+" -> ");
-			tempNode = tempNode.getNext();
-		}
-		System.out.println("null");
+		myLinkedList.display();
+		
 	}
 }
